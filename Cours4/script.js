a71e040db41633606e1c0911b50ec18f5b2b1c29
ilanhassan <36660434+ilanhassan@users.noreply.github.com>
2018-04-23 21:48:20 +0200

    Cours4

---
+++ Cours4/script.js
@@ -0,0 +1,59 @@
+$( document ).ready(function() {
+  alert('Hello World')
+
+function welcome() {
+   $("#welcome").html('<h4>Bienvenue à tous, <br> soyez pret à renouveler de nouveaux défis !</h4>');
+};
+
+
+function getUsername() {
+    //affiche une boite de dialogue 
+    var  user = prompt("Il est temps de rentrer votre nom ou surnom:", "");
+    // la fonction se termine en retournant le nom récupéré par la boite de dialogue
+    return user
+};
+
+ function welcome() {
+    var name= getUsername();
+    $("#welcome").html('<h4>Bienvenue '+name+', <br> pas une minute à perdre : soyez prêt à renouveler de nouveaux défis ! </h4>');
+
+};
+// Appel la fonction
+welcome();
+
+function loadChallenges() {
+  $.ajax(
+        {url : 'https://s3.eu-central-1.amazonaws.com/spintransfer/challenge.json',
+        type: 'GET',
+        dataType: 'json'} 
+  )
+  .done(function(data) {
+    showChallenge(data);
+  })
+  .fail(function() {
+    alert( "error" );
+  });
+};
+
+function showChallenge(liste) {
+
+  var data="";
+
+  for ( var i=0; i < liste.length; i++) {
+    data+='<div class="defi">';
+
+    var challenge=liste[i];
+
+    data+='<h2>'+challenge.nom+'</h2>';
+    data+='<p>'+challenge.description+'</p>';
+    data+='<iframe width="364" height="204" src="'+challenge.youtube+'" frameborder="0" allowfullscreen=""></iframe>'
+    data+="</div>";
+  }
+  $("#defis").html(data);
+
+};
+
+loadChallenges();
+
+})
+
